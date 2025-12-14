d3277c2d405f0c4d4ebbf185034b103fe271ccbb
Piyush Tripathi <piyushtripathi95@gmail.com>
2025-12-14 10:13:12 +0530

    project 2 - assignment 1

---
+++ File 1/project-2/src/assignment/Assignment1.jsx
@@ -0,0 +1,11 @@
+function Assignment1(){
+  const name = "Piyush";
+  return(
+    <div>
+      <h2>Assignment 1</h2>
+      <h3>Hello,{name}</h3>
+
+    </div>
+  );
+}
+export default Assignment1;
