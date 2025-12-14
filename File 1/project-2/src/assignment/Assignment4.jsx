758384a7923c11d29d050cf937709ccb8c132111
Piyush Tripathi <piyushtripathi95@gmail.com>
2025-12-14 10:31:34 +0530

    Project 2 - Assignment 4

---
+++ File 1/project-2/src/assignment/Assignment4.jsx
@@ -0,0 +1,11 @@
+function Assignment4(){
+  return(
+    <div>
+      <h3>Assignment 4</h3>
+      <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/500px-Image_created_with_a_mobile_phone.png"
+      alt="Sample"/>
+    </div>
+  );
+}
+export default Assignment4;
