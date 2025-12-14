247cc6bf63a6a3b794e7905603c06c506bd9981a
Piyush Tripathi <piyushtripathi95@gmail.com>
2025-12-14 11:24:41 +0530

    Project 3 - Assignment 2

--- File 1/project-3/src/App.jsx
+++ File 1/project-3/src/App.jsx
@@ -1,9 +1,18 @@
 import Assignment1 from "./assignment/Assignment1";
+import Assignment2 from "./assignment/Assignment2";
 
 function App(){
   return(
     <div>
       <Assignment1 name = "Piyush"/>
+      <hr/>
+
+      <Assignment2
+      name="Piyush"
+      role="SDE"
+      city="Bhopal"
+      />
+      <hr/>
 
     </div>
   );
---
+++ File 1/project-3/src/assignment/Assignment2.jsx
@@ -0,0 +1,12 @@
+function Assignment2({name,role,city}){
+  return(
+    <div>
+      <h3>Assignment 2</h3>
+      <h4>Name:{name}</h4>
+      <p>Role:{role}</p>
+      <p>City:{city}</p>
+
+    </div>
+  );
+}
+export default Assignment2;
