be84da79d9ea5f3c55dae30d881bbc05aa0f522d
Piyush Tripathi <piyushtripathi95@gmail.com>
2025-12-14 17:27:32 +0530

    ProfileCard using props

--- File 1/project-4/src/App.jsx
+++ File 1/project-4/src/App.jsx
@@ -1,1 +1,28 @@
+import ProfileCard from "./componets/ProfileCard";
+
+function App(){
+  return(
+    <div>
+      <ProfileCard
+      name="Name1"
+      role="Role1"
+      city="City1"
+      />
+      <hr/>
+      <ProfileCard
+      name="Name2"
+      role="Role2"
+      city="City2"
+      />
+      <hr/>
+      <ProfileCard
+      name="Name3"
+      role="Role3"
+      city="City3"
+      />
+      <hr/>
+
+    </div>
+  );
+}
 export default App
---
+++ File 1/project-4/src/componets/ProfileCard.jsx
@@ -0,0 +1,12 @@
+function ProfileCard({name,role,city}){
+  return(
+    <div>
+      <h4>Name:{name}</h4>
+      <p>Role:{role}</p>
+      <p>City:{city}</p>
+
+    </div>
+
+  );
+}
+export default ProfileCard;
