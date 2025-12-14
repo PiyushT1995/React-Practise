3cdc5697d5aab7d23a5ecd64d17482409417898a
Piyush Tripathi <piyushtripathi95@gmail.com>
2025-12-14 07:35:12 +0530

    Project 1 - Assignment 2

--- File 1/my-react-app/src/App.jsx
+++ File 1/my-react-app/src/App.jsx
@@ -1,9 +1,12 @@
 import Assignment1 from "./assignments/Assignment1";
+import Assignment2 from "./assignments/Assignment2";
 function App()
 {
   return(
     <div>
       <Assignment1/>
+      <hr/>
+      <Assignment2/>
       <hr/>
       
 
---
+++ File 1/my-react-app/src/assignments/Assignment2.jsx
@@ -0,0 +1,16 @@
+function Assignment2() {
+
+  return(
+    <div>
+      <h3>Assignment:2</h3>
+      <h4>My First React App</h4>
+      <p>
+         React is a JavaScript library used to build user interfaces.
+         It helps create fast and interactive web applications using components.
+      </p>
+
+    </div>
+
+  );
+}
+export default Assignment2;
