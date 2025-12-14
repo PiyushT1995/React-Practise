a2cd6c70923d6d88e80c3c4fe7198c1880d4c930
Piyush Tripathi <piyushtripathi95@gmail.com>
2025-12-14 11:10:48 +0530

    Project 3 - Assignment 1

--- File 1/project-3/src/App.jsx
+++ File 1/project-3/src/App.jsx
@@ -1,1 +1,11 @@
+import Assignment1 from "./assignment/Assignment1";
+
+function App(){
+  return(
+    <div>
+      <Assignment1 name = "Piyush"/>
+
+    </div>
+  );
+} 
 export default App
---
+++ File 1/project-3/src/assignment/Assignment1.jsx
@@ -0,0 +1,9 @@
+function Assignment1(props){
+  return(
+    <div>
+      <h3>Assignment 1</h3>
+      <p>Hello,{props.name}</p>
+    </div>
+  );
+}
+export default Assignment1;
