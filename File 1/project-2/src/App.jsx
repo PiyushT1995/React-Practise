971d515bd467eea2531ecbbdadda37c882235878
Piyush Tripathi <piyushtripathi95@gmail.com>
2025-12-14 10:36:25 +0530

    Project 2 - Assignment 5

--- File 1/project-2/src/App.jsx
+++ File 1/project-2/src/App.jsx
@@ -2,6 +2,7 @@ import Assignment1 from "./assignment/Assignment1";
 import Assignment2 from "./assignment/Assignment2";
 import Assignment3 from "./assignment/Assignment3";
 import Assignment4 from "./assignment/Assignment4";
+import Assignment5 from "./assignment/Assignment5";
 
 function App() {
   return(
@@ -14,6 +15,7 @@ function App() {
       <hr/>
       <Assignment4/>
       <hr/>
+      <Assignment5/>
 
     </div>
 
---
+++ File 1/project-2/src/assignment/Assignment5.jsx
@@ -0,0 +1,9 @@
+function Assignment5(){
+  return(
+    <div>
+      <h3>Assignment 5</h3>
+      <p>This is my practise</p>
+    </div>
+  );
+}
+export default Assignment5;
