965230a12c3bec98093d64daa8e4569970e5f99c
Piyush Tripathi <piyushtripathi95@gmail.com>
2025-12-14 10:18:30 +0530

    Project 2 - Assignment 2

--- File 1/project-2/src/App.jsx
+++ File 1/project-2/src/App.jsx
@@ -1,9 +1,12 @@
 import Assignment1 from "./assignment/Assignment1";
+import Assignment2 from "./assignment/Assignment2";
 
 function App() {
   return(
     <div>
       <Assignment1/>
+      <hr/>
+      <Assignment2/>
       <hr/>
 
     </div>
---
+++ File 1/project-2/src/assignment/Assignment2.jsx
@@ -0,0 +1,12 @@
+function Assignment2(){
+  const num1 = 10;
+  const num2 = 20;
+  return(
+    <div>
+      <h2>Assignment 2</h2>
+      <p>Sum of two numbers: {num1+num2}</p>
+
+    </div>
+  );
+}
+export default Assignment2;
