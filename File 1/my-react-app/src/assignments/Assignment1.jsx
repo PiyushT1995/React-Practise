58b58159855b696e477e823956dcaaea6a8339b2
Piyush Tripathi <piyushtripathi95@gmail.com>
2025-12-14 07:24:25 +0530

    Project 1 - Assignment 1

---
+++ File 1/my-react-app/src/assignments/Assignment1.jsx
@@ -0,0 +1,15 @@
+function Assignment1(){
+  return(
+    <div>
+      <h3>Assignment 1</h3>
+      <p>Name:Piyush</p>
+      <p>Age:31</p>
+      <p>City:Bhopal</p>
+
+
+
+    </div>
+
+  );
+}
+export default Assignment1;
